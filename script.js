cbc177dfc6300b735d271825cbb02f8f3f43b3e5
Vuthikorn DR.eatha <vuthikorpannakh@gmail.com>
2026-05-17 14:34:48 +0700

    เพิ่มองประกอบเว็ปให้ครบและตกแต่งเสริมลูกเล่นนิดหน่อย

---
+++ script.js
@@ -0,0 +1,40 @@
+const tab1 = document.getElementById('nav-tab1')
+const tab2 = document.getElementById('nav-tab2')
+
+let isOverTab1 = false;
+let isOverTab2 = false;
+
+function checkandHide() {
+    setTimeout(() => {
+        if (!isOverTab1 && !isOverTab2) {
+            tab2.classList.add('nav-2-hidden');
+        }
+    }, 400);
+}
+
+/* SHOW */
+
+tab1.addEventListener('mouseenter', () => {
+    isOverTab1 = true;
+    tab2.classList.remove('nav-2-hidden');
+});
+
+/* LEAVE TAB1 */
+
+tab1.addEventListener('mouseleave', () => {
+    isOverTab1 = false;
+    checkandHide();
+});
+
+/* ENTER TAB2 */
+
+tab2.addEventListener('mouseenter', () => {
+    isOverTab2 = true;
+});
+
+/* LEAVE TAB2 */
+
+tab2.addEventListener('mouseleave', () => {
+    isOverTab2 = false;
+    checkandHide();
+});
