297ceb0fc5a89ab06fbea3f0007633ab8fe09306
fumisaki-g <vuthikorpannakh@gmail.com>
2026-05-31 12:38:09 +0700

    เพิ่มเติมเรื่องการจัดวางข้อมูลมีกรใสรายละเอียดเพิ่มและขึ้นโครงสร้างหน้าผลงาน

---
+++ assets/js/popup.js
@@ -0,0 +1,42 @@
+const openpopupc1 = document.getElementById('open-popup-c1');
+const closepopupc1 = document.getElementById('close-popup-c1');
+const backgroundpopupc1 = document.getElementById('background-popup-c1');
+
+const openpopupc2 = document.getElementById('open-popup-c2');
+const closepopupc2 = document.getElementById('close-popup-c2');
+const backgroundpopupc2 = document.getElementById('background-popup-c2');
+
+const openpopupc3 = document.getElementById('open-popup-c3');
+const closepopupc3 = document.getElementById('close-popup-c3');
+const backgroundpopupc3 = document.getElementById('background-popup-c3');
+
+function openPopupc1() {
+    backgroundpopupc1.style.display = "flex";
+}
+function closePopupc1() {
+    backgroundpopupc1.style.display = "none";
+}
+
+
+function openPopupc2() {
+    backgroundpopupc2.style.display = "flex";
+}
+function closePopupc2() {
+    backgroundpopupc2.style.display = "none";
+}
+
+
+function openPopupc3() {
+    backgroundpopupc3.style.display = "flex";
+}
+function closePopupc3() {
+    backgroundpopupc3.style.display = "none";
+}
+
+
+openpopupc1.addEventListener('click', openPopupc1);
+closepopupc1.addEventListener('click', closePopupc1);
+openpopupc2.addEventListener('click', openPopupc2);
+closepopupc2.addEventListener('click', closePopupc2);
+openpopupc3.addEventListener('click', openPopupc3);
+closepopupc3.addEventListener('click', closePopupc3);
