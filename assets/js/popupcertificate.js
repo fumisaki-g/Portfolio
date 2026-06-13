5603b58b9d9311a251b80a23ad66b7e80711550b
fumisaki-g <vuthikorpannakh@gmail.com>
2026-06-13 22:37:11 +0700

    เพิ่มรายละเอียดและลงป้อปอัพทั้งหมดในส่วนของเกียรติบัตรลงไป

--- assets/js/popupcertificate.js
+++ assets/js/popupcertificate.js
@@ -26,3 +26,445 @@ function closePopup2() {
 openpopup2.addEventListener('click', openPopup2);
 closepopup2.addEventListener('click', closePopup2);
 
+const openpopup3 = document.getElementById ('open-popup-certificate-3');
+const closepopup3 = document.getElementById('close-popup-certificate-3');
+const backgroundpopup3 = document.getElementById('background-popup-certificate-3');
+
+function openPopup3() {
+    backgroundpopup3.style.display = "flex";
+}
+function closePopup3() {
+    backgroundpopup3.style.display = "none";
+}
+
+openpopup3.addEventListener('click', openPopup3);
+closepopup3.addEventListener('click', closePopup3);
+
+const openpopup4 = document.getElementById('open-popup-certificate-4');
+const closepopup4 = document.getElementById('close-popup-certificate-4');
+const backgroundpopup4 = document.getElementById('background-popup-certificate-4');
+
+function openPopup4() {
+    backgroundpopup4.style.display = "flex";
+}
+function closePopup4() {
+    backgroundpopup4.style.display = "none";
+}
+
+openpopup4.addEventListener('click', openPopup4);
+closepopup4.addEventListener('click', closePopup4);
+
+const openpopup5 = document.getElementById('open-popup-certificate-5');
+const closepopup5 = document.getElementById('close-popup-certificate-5');
+const backgroundpopup5 = document.getElementById('background-popup-certificate-5');
+
+function openPopup5() {
+    backgroundpopup5.style.display = "flex";
+}
+
+function closePopup5() {
+    backgroundpopup5.style.display = "none";
+}
+
+openpopup5.addEventListener('click', openPopup5);
+closepopup5.addEventListener('click', closePopup5);
+
+const openpopups1 = document.getElementById('open-popup-certificate-s1');
+const closepopups1 = document.getElementById('close-popup-certificate-s1');
+const backgroundpopups1 = document.getElementById('background-popup-certificate-s1');
+
+function openPopups1() {
+    backgroundpopups1.style.display = "flex";
+}   
+function closePopups1() {
+    backgroundpopups1.style.display = "none";
+}
+
+openpopups1.addEventListener('click', openPopups1);
+closepopups1.addEventListener('click', closePopups1);
+
+
+const openpopups2 = document.getElementById('open-popup-certificate-s2');
+const closepopups2 = document.getElementById('close-popup-certificate-s2');
+const backgroundpopups2 = document.getElementById('background-popup-certificate-s2');
+
+function openPopups2() {
+    backgroundpopups2.style.display = "flex";
+}
+function closePopups2() {
+    backgroundpopups2.style.display = "none";
+}
+
+openpopups2.addEventListener('click', openPopups2);
+closepopups2.addEventListener('click', closePopups2);
+
+
+
+const openpopups3 = document.getElementById('open-popup-certificate-s3');
+const closepopups3 = document.getElementById('close-popup-certificate-s3');
+const backgroundpopups3 = document.getElementById('background-popup-certificate-s3');
+
+function openPopups3() {
+    backgroundpopups3.style.display = "flex";
+}
+function closePopups3() {
+    backgroundpopups3.style.display = "none";
+}
+
+openpopups3.addEventListener('click', openPopups3);
+closepopups3.addEventListener('click', closePopups3);
+
+
+const openpopups4 = document.getElementById('open-popup-certificate-s4');
+const closepopups4 = document.getElementById('close-popup-certificate-s4');
+const backgroundpopups4 = document.getElementById('background-popup-certificate-s4');
+
+function openPopups4() {
+    backgroundpopups4.style.display = "flex";
+}
+function closePopups4() {
+    backgroundpopups4.style.display = "none";
+}
+
+openpopups4.addEventListener('click', openPopups4);
+closepopups4.addEventListener('click', closePopups4);
+
+
+const openpopups5 = document.getElementById('open-popup-certificate-s5');
+const closepopups5 = document.getElementById('close-popup-certificate-s5');
+const backgroundpopups5 = document.getElementById('background-popup-certificate-s5');
+
+function openPopups5() {
+    backgroundpopups5.style.display = "flex";
+}
+function closePopups5() {
+    backgroundpopups5.style.display = "none";
+}
+
+openpopups5.addEventListener('click', openPopups5);
+closepopups5.addEventListener('click', closePopups5);
+
+
+const openpopups6 = document.getElementById('open-popup-certificate-s6');
+const closepopups6 = document.getElementById('close-popup-certificate-s6');
+const backgroundpopups6 = document.getElementById('background-popup-certificate-s6');
+
+function openPopups6() {
+    backgroundpopups6.style.display = "flex";
+}
+function closePopups6() {
+    backgroundpopups6.style.display = "none";
+}
+
+openpopups6.addEventListener('click', openPopups6);
+closepopups6.addEventListener('click', closePopups6);
+
+
+const openpopups7 = document.getElementById('open-popup-certificate-s7');
+const closepopups7 = document.getElementById('close-popup-certificate-s7');
+const backgroundpopups7 = document.getElementById('background-popup-certificate-s7');
+
+function openPopups7() {
+    backgroundpopups7.style.display = "flex";
+}
+function closePopups7() {
+    backgroundpopups7.style.display = "none";
+}
+
+openpopups7.addEventListener('click', openPopups7);
+closepopups7.addEventListener('click', closePopups7);
+
+
+const openpopups8 = document.getElementById('open-popup-certificate-s8');
+const closepopups8 = document.getElementById('close-popup-certificate-s8');
+const backgroundpopups8 = document.getElementById('background-popup-certificate-s8');
+function openPopups8() {
+    backgroundpopups8.style.display = "flex";
+}
+function closePopups8() {
+    backgroundpopups8.style.display = "none";
+}
+openpopups8.addEventListener('click', openPopups8);
+closepopups8.addEventListener('click', closePopups8);
+
+
+const openpopups9 = document.getElementById('open-popup-certificate-s9');
+const closepopups9 = document.getElementById('close-popup-certificate-s9');
+const backgroundpopups9 = document.getElementById('background-popup-certificate-s9');
+function openPopups9() {
+    backgroundpopups9.style.display = "flex";
+}
+function closePopups9() {
+    backgroundpopups9.style.display = "none";
+}
+openpopups9.addEventListener('click', openPopups9);
+closepopups9.addEventListener('click', closePopups9);
+
+
+
+const openpopups10 = document.getElementById('open-popup-certificate-s10');
+const closepopups10 = document.getElementById('close-popup-certificate-s10');
+const backgroundpopups10 = document.getElementById('background-popup-certificate-s10');
+function openPopups10() {
+    backgroundpopups10.style.display = "flex";
+}
+function closePopups10() {
+    backgroundpopups10.style.display = "none";
+}
+openpopups10.addEventListener('click', openPopups10);
+closepopups10.addEventListener('click', closePopups10);
+
+
+
+const openpopups11 = document.getElementById('open-popup-certificate-s11');
+const closepopups11 = document.getElementById('close-popup-certificate-s11');
+const backgroundpopups11 = document.getElementById('background-popup-certificate-s11');
+function openPopups11() {
+    backgroundpopups11.style.display = "flex";
+}
+
+function closePopups11() {
+    backgroundpopups11.style.display = "none";
+}
+
+openpopups11.addEventListener('click', openPopups11);
+closepopups11.addEventListener('click', closePopups11);
+
+
+
+const openpopups12 = document.getElementById('open-popup-certificate-s12');
+const closepopups12 = document.getElementById('close-popup-certificate-s12');
+const backgroundpopups12 = document.getElementById('background-popup-certificate-s12');
+function openPopups12() {
+    backgroundpopups12.style.display = "flex";
+}
+
+function closePopups12() {
+    backgroundpopups12.style.display = "none";
+}
+
+openpopups12.addEventListener('click', openPopups12);
+closepopups12.addEventListener('click', closePopups12);
+
+
+const openpopups13 = document.getElementById('open-popup-certificate-s13');
+const closepopups13 = document.getElementById('close-popup-certificate-s13');
+const backgroundpopups13 = document.getElementById('background-popup-certificate-s13');
+function openPopups13() {
+    backgroundpopups13.style.display = "flex";
+}
+
+function closePopups13() {
+    backgroundpopups13.style.display = "none";
+}
+
+openpopups13.addEventListener('click', openPopups13);
+closepopups13.addEventListener('click', closePopups13);
+
+
+const openpopups14 = document.getElementById('open-popup-certificate-s14');
+const closepopups14 = document.getElementById('close-popup-certificate-s14');
+const backgroundpopups14 = document.getElementById('background-popup-certificate-s14');
+function openPopups14() {
+    backgroundpopups14.style.display = "flex";
+}
+
+function closePopups14() {
+    backgroundpopups14.style.display = "none";
+}
+
+openpopups14.addEventListener('click', openPopups14);
+closepopups14.addEventListener('click', closePopups14);
+
+
+
+const openpopups15 = document.getElementById('open-popup-certificate-s15');
+const closepopups15 = document.getElementById('close-popup-certificate-s15');
+const backgroundpopups15 = document.getElementById('background-popup-certificate-s15');
+function openPopups15() {
+    backgroundpopups15.style.display = "flex";
+}
+
+function closePopups15() {
+    backgroundpopups15.style.display = "none";
+}
+
+openpopups15.addEventListener('click', openPopups15);
+closepopups15.addEventListener('click', closePopups15);
+
+
+
+const openpopups16 = document.getElementById('open-popup-certificate-s16');
+const closepopups16 = document.getElementById('close-popup-certificate-s16');
+const backgroundpopups16 = document.getElementById('background-popup-certificate-s16');
+function openPopups16() {
+    backgroundpopups16.style.display = "flex";
+}
+
+function closePopups16() {
+    backgroundpopups16.style.display = "none";
+}
+openpopups16.addEventListener('click', openPopups16);
+closepopups16.addEventListener('click', closePopups16);
+
+
+
+const openpopups17 = document.getElementById('open-popup-certificate-s17');
+const closepopups17 = document.getElementById('close-popup-certificate-s17');
+const backgroundpopups17 = document.getElementById('background-popup-certificate-s17');
+function openPopups17() {
+    backgroundpopups17.style.display = "flex";
+}
+
+function closePopups17() {
+    backgroundpopups17.style.display = "none";
+}
+
+openpopups17.addEventListener('click', openPopups17);
+closepopups17.addEventListener('click', closePopups17);
+
+
+
+const openpopups18 = document.getElementById('open-popup-certificate-s18');
+const closepopups18 = document.getElementById('close-popup-certificate-s18');
+const backgroundpopups18 = document.getElementById('background-popup-certificate-s18');
+function openPopups18() {
+    backgroundpopups18.style.display = "flex";
+}
+
+function closePopups18() {
+    backgroundpopups18.style.display = "none";
+}
+
+openpopups18.addEventListener('click', openPopups18);
+closepopups18.addEventListener('click', closePopups18);
+
+
+
+const openpopups19 = document.getElementById('open-popup-certificate-s19');
+const closepopups19 = document.getElementById('close-popup-certificate-s19');
+const backgroundpopups19 = document.getElementById('background-popup-certificate-s19');
+function openPopups19() {
+    backgroundpopups19.style.display = "flex";
+}
+
+function closePopups19() {
+    backgroundpopups19.style.display = "none";
+}
+
+openpopups19.addEventListener('click', openPopups19);
+closepopups19.addEventListener('click', closePopups19);
+
+
+
+const openpopups20 = document.getElementById('open-popup-certificate-s20');
+const closepopups20 = document.getElementById('close-popup-certificate-s20');
+const backgroundpopups20 = document.getElementById('background-popup-certificate-s20');
+function openPopups20() {
+    backgroundpopups20.style.display = "flex";
+}
+
+function closePopups20() {
+    backgroundpopups20.style.display = "none";
+}
+
+openpopups20.addEventListener('click', openPopups20);
+closepopups20.addEventListener('click', closePopups20);
+
+
+
+const openpopups21 = document.getElementById('open-popup-certificate-s21');
+const closepopups21 = document.getElementById('close-popup-certificate-s21');
+const backgroundpopups21 = document.getElementById('background-popup-certificate-s21');
+function openPopups21() {
+    backgroundpopups21.style.display = "flex";
+}
+
+function closePopups21() {
+    backgroundpopups21.style.display = "none";
+}
+
+openpopups21.addEventListener('click', openPopups21);
+closepopups21.addEventListener('click', closePopups21);
+
+
+
+const openpopups22 = document.getElementById('open-popup-certificate-s22');
+const closepopups22 = document.getElementById('close-popup-certificate-s22');
+const backgroundpopups22 = document.getElementById('background-popup-certificate-s22');
+function openPopups22() {
+    backgroundpopups22.style.display = "flex";
+}
+
+function closePopups22() {
+    backgroundpopups22.style.display = "none";
+}
+
+openpopups22.addEventListener('click', openPopups22);
+closepopups22.addEventListener('click', closePopups22);
+
+
+
+const openpopups23 = document.getElementById('open-popup-certificate-s23');
+const closepopups23 = document.getElementById('close-popup-certificate-s23');
+const backgroundpopups23 = document.getElementById('background-popup-certificate-s23');
+function openPopups23() {
+    backgroundpopups23.style.display = "flex";
+}
+
+function closePopups23() {
+    backgroundpopups23.style.display = "none";
+}
+
+openpopups23.addEventListener('click', openPopups23);
+closepopups23.addEventListener('click', closePopups23);
+
+
+
+const openpopups24 = document.getElementById('open-popup-certificate-s24');
+const closepopups24 = document.getElementById('close-popup-certificate-s24');
+const backgroundpopups24 = document.getElementById('background-popup-certificate-s24');
+function openPopups24() {
+    backgroundpopups24.style.display = "flex";
+}
+
+function closePopups24() {
+    backgroundpopups24.style.display = "none";
+}
+
+openpopups24.addEventListener('click', openPopups24);
+closepopups24.addEventListener('click', closePopups24);
+6
+
+const openpopups25 = document.getElementById('open-popup-certificate-s25');
+const closepopups25 = document.getElementById('close-popup-certificate-s25');
+const backgroundpopups25 = document.getElementById('background-popup-certificate-s25');
+function openPopups25() {
+    backgroundpopups25.style.display = "flex";
+}
+
+function closePopups25() {
+    backgroundpopups25.style.display = "none";
+}
+
+openpopups25.addEventListener('click', openPopups25);
+closepopups25.addEventListener('click', closePopups25);
+
+
+
+const openpopups26 = document.getElementById('open-popup-certificate-s26');
+const closepopups26 = document.getElementById('close-popup-certificate-s26');
+const backgroundpopups26 = document.getElementById('background-popup-certificate-s26');
+function openPopups26() {
+    backgroundpopups26.style.display = "flex";
+}
+
+function closePopups26() {
+    backgroundpopups26.style.display = "none";
+}
+
+openpopups26.addEventListener('click', openPopups26);
+closepopups26.addEventListener('click', closePopups26);
+
+
+
