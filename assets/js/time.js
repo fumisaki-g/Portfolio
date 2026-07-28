264d869b6623d82e56b0155d142f4fd2c290f76e
Vuthikorn DR.eatha <vuthikorpannakh@gmail.com>
2026-07-28 22:51:13 +0700

    Update time.js
    Update เวลากิจกรรม

--- assets/js/time.js
+++ assets/js/time.js
@@ -1,5 +1,5 @@
 // กำหนดวันและเวลาที่ต้องการให้นับถอยหลังไปถึง
-const targetDate = new Date("2026-08-15T00:00:00").getTime();
+const targetDate = new Date("2026-08-5T00:00:00").getTime();
 
 
 function updateCountdown() {
