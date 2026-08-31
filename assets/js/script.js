f0f8cad7d6a68a9cedf2d66c5fa5fdb8bb34b4a9
fumisaki-g <vuthikorpannakh@gmail.com>
2026-08-31 21:45:23 +0700

    อัปเดตวันที่สิ้นสุดโครงการในไฟล์ timeline.json และปรับเปลี่ยนวันนับถอยหลังใน script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -819,7 +819,7 @@ window.addEventListener('load', function() {
 
 // timeline
 // กำหนดวันและเวลาที่ต้องการให้นับถอยหลังไปถึง
-const targetDate = new Date("2026-10-01T09:30:00").getTime();
+const targetDate = new Date("2026-09-10T09:30:00").getTime();
 
 
 function updateCountdown() {
